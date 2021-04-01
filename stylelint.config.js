d61ed2539750e913de4810f83011371441523729
horri1520 <kh1520.shsf@gmail.com>
2021-04-02 04:09:22 +0900

    feat: introduce stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-recommended-scss',
+    'stylelint-scss',
+  ],
+  rules: {},
+};
