68fcb6fc586f1ad300a079c5c9d1817a093ff791
horri1520 <kh1520.shsf@gmail.com>
2021-04-01 20:15:10 +0900

    test: husky run check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,8 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
+        <p>test</p>
+
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
